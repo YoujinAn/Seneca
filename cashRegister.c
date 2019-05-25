9d8dd11cd40559db5b691120631655711a5b96a5
YoujinAn <50999362+YoujinAn@users.noreply.github.com>
2019-05-24 21:42:31 -0400

    Add files via upload

--- cashRegister.c
+++ cashRegister.c
@@ -2,34 +2,57 @@
 // Name:           Youjin An
 // Student Number: 140413188
 // Email:          yan24@myseneca.ca
-// Section:        SAA
-// Workshop:       2 (in-lab)
+// Section:        SII
+// Workshop:       2 (at-home)
 //----------------------------------------------
 
 #define _CRT_SECURE_NO_WARNINGS
 
+// Paste your in-lab code below and upgrade it to the at-home specifications
 // Start your code below:
 
 #include <stdio.h>
 
 int main(void)
 {
-	double amount, balance;
-	int loonies, quarter;
+	double GTS;
+	double amount;
+	int loonies, quarter, dimes, nickels, pennies, total, balance;
 
-	/* Accept data from a user */
-	printf("Please enter the amount to be paid: $");
+	// Accept data from a user
+	printf("Please enter the amount to be paid: $ ");
 	scanf("%lf", &amount);
 
+	// GST information
+	GTS = (amount * 0.13) + 0.005;
+	printf("GST: %.2lf\n", GTS); // 1.13
+	total = (amount + GTS) * 100;
+	printf("Balance owing: $ %.2f\n", amount); //
 
-	loonies = (int) amount;
-	balance = amount - loonies;
-	printf("Loonies required: %d, balance owing $%.2lf\n", loonies, balance);
+	// Loonies 
+	loonies = total / 100;
+	balance = total % 100;
+	printf("Loonies required: %d, balance owing $ %.2lf\n", loonies, (float)balance / 100);
 
+	// Quarters
+	quarter = balance / 25;
+	balance = balance % 25;
+	printf("Quarters required: %d, balance owing $ %.2lf\n", quarter, (float)balance / 100);
 
-	quarter = (int)(balance / 0.25);
-	balance = balance - (quarter * 0.25);
-	printf("Quarters required: %d, balance owing $%.2lf\n", quarter, balance);
+	// Dimes
+	dimes = balance / 5;
+	balance = balance % 10;
+	printf("Dimes required: %d, balance owing $ %.2lf\n", dimes, (float)balance / 100);
+
+	// Nickels
+	nickels = balance / 5;
+	balance = balance % 5;
+	printf("Nickels required: %d, balance owing $ %.2lf\n", nickels, (float)balance / 100);
+
+	// Pennies
+	pennies = balance / 1;
+	balance = balance % 1;
+	printf("Pennies required: %d, balance owing $ %.2lf\n", pennies, (float)balance / 100);
 
 
 	return 0;
