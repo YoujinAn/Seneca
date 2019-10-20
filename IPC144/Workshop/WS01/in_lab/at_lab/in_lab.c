f4145bd06b6c3a6a43587fae220f1e0e169cb62c
Rene <auj0827@gmail.com>
2019-10-20 12:48:06 -0400

    Update

---
+++ IPC144/Workshop/WS01/in_lab/at_lab/in_lab.c
@@ -0,0 +1,18 @@
+/***********************************************************************
+// IPC144 Workshop 1 Getting Started
+// File in_lab.c
+
+	▶ Name: Youjin An
+	▶ Part: in_lab
+	▶ Date: 2019/05/31
+
+***********************************************************************/
+
+#include <stdio.h>
+
+int main() {
+	// For the in-lab part you are to write a C program that displays
+	printf("*** Welcome to C Programming ***");
+
+	return 0;
+}
