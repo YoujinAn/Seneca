dc3df45d3e7a3cb08be0950535a86e9e12be4737
YoujinAn <auj0827@gmail.com>
2020-02-02 01:11:43 -0500

    [oop345] WS03 home

---
+++ OOP345/1. Workshop/WS03/at-home/at_home/Pair.h
@@ -0,0 +1,57 @@
+// Name: Youjin An
+// Seneca Student ID:140413188
+// Seneca email:yan24@myseneca.ca
+// Date of completion:01 Feb, 2020
+
+#ifndef SDDS_PAIR_H__
+#define SDDS_PAIR_H__
+
+#include <iostream>
+
+namespace sdds {
+	// `K`: the type of the key
+	// `V`: the type of the value
+	template <typename K, typename V>
+
+	class Pair {
+
+		K _key;
+		V _val;
+
+	public:
+		// default constructor
+		Pair() {
+			_key = {};
+			_val = {};
+		}
+
+		// copies the values received in the parameters into instance variables
+		Pair(const K& key, const V& value) {
+			_key = key;
+			_val = value;
+		}
+
+		// returns the **key** component of the pair
+		const K& key() const {
+			return _key;
+		}
+
+		// returns the **value** component of the pair
+		const V& value() const {
+			return _val;
+		}
+
+		// inserts into the parameter the key and value of the pair in the following format
+		virtual void display(std::ostream& os) const {
+			os << key() << " : " << value() << std::endl;
+		}
+	};
+
+	// calls the function `Pair<L, V>::display()` to insert a pair into the stream.
+	template<typename K, typename V>
+	std::ostream& operator<<(std::ostream& os, const Pair<K, V>& pair) {
+		pair.display(os);
+		return os;
+	}
+}
+#endif // SDDS_PAIR_H__
