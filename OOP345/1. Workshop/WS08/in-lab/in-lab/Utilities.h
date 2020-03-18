a465f1e34d4c903a7f9a1c3c349a3fa47eb2a615
YoujinAn <auj0827@gmail.com>
2020-03-18 11:21:12 -0400

    [oop345]
    WS08/in-lab/

---
+++ OOP345/1. Workshop/WS08/in-lab/in-lab/Utilities.h
@@ -0,0 +1,18 @@
+// Name: Youjin An
+// Seneca Student ID:140413188
+// Seneca email:yan24@myseneca.ca
+// Date of completion:18 March, 2020
+
+// Workshop 8 - Smart Pointers
+// 2019/11 - Cornel
+
+#ifndef SDDS_UTILITIES_H
+#define SDDS_UTILITIES_H
+
+#include "List.h"
+#include "Element.h"
+
+namespace sdds {
+	List<Product> mergeRaw(const List<Description>& desc, const List<Price>& price);
+}
+#endif
