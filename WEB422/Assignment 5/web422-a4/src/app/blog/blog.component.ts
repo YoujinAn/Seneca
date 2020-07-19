43ffa82259ae7b60fef4f0861b2453c9dc527400
Rene An <auj0827@gmail.com>
2020-07-19 15:03:28 -0400

    Step 4: Refactoring the "Blog" view (BlogComponent)

--- WEB422/Assignment 5/web422-a4/src/app/blog/blog.component.ts
+++ WEB422/Assignment 5/web422-a4/src/app/blog/blog.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import blogData from '../blogData.json';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PostService } from '../post.service';
+import { ActivatedRoute } from '@angular/router';
 import { BlogPost } from '../BlogPost';
 
 @Component({
@@ -7,13 +8,51 @@ import { BlogPost } from '../BlogPost';
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
 })
+
 export class BlogComponent implements OnInit {
 
-  blogPosts: Array<BlogPost> = blogData;
-  
-  constructor() { }
+  blogPosts: Array<BlogPost>;
+  page: number = 1;
+  tag: string = null;
+  category: string = null;
+  querySub: any;
 
-  ngOnInit(): void {
+  constructor(private data: PostService, private route: ActivatedRoute) { }
+
+  getPage(num) {
+    console.log(this.tag);
+    console.log(this.category);
+    this.querySub = this.data.getPosts(num, this.tag, this.category).subscribe(data => {
+
+      if (data.length > 0) {
+        this.blogPosts = data;
+        this.page = num;
+      }
+    });
   }
 
+  ngOnInit(): void {
+
+    this.querySub = this.route.queryParams.subscribe(params => {
+      if (params['tag']) {
+        this.tag = params['tag'];
+        this.category = null;
+      } else {
+        this.tag = null;
+      }
+      if (params['category']) {
+        this.category = params['category'];
+        this.tag = null;
+      } else {
+        this.category = null;
+      }
+      this.getPage(+params['page'] || 1);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.querySub) {
+      this.querySub.unsubscribe();
+    }
+  }
 }
