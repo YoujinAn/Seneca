a849f6a80b0415b0a4633175fb6732d0c593c688
Rene An <auj0827@gmail.com>
2020-07-22 21:03:19 -0400

    WS08

---
+++ JAC444/WS08/src/student/Student.java
@@ -0,0 +1,127 @@
+/*
+ * JAC444 Workshop 8
+ * File: Student.java
+ * Name: Youjin An (140413188)
+ * Date: July 22, 2020
+ */
+
+package student;
+
+public class Student {
+
+	// Fields
+	private String firstName;
+	private String lastName;
+	private double grade;
+	private String department;
+
+	// Constructor
+	public Student(String firstName, String lastName, double grade, String department) {
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+		this.setGrade(grade);
+		this.setDepartment(department);
+	}
+
+	// Full Name
+	public String getName() {
+		return getFirstName() + " " + getLastName();
+	}
+
+	// fistName
+	public void setFirstName(String _firstName) {
+		this.firstName = _firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	// lastName
+	public void setLastName(String _lastName) {
+		this.lastName = _lastName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	// grade
+	public void setGrade(double _grade) {
+		this.grade = _grade;
+	}
+
+	public double getGrade() {
+		return grade;
+	}
+
+	// department
+	public void setDepartment(String _department) {
+		this.department = _department;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+	
+	// ToString
+	@Override
+	public String toString() {
+		return String.format("%-9s%-9s%8.2f   %s", getFirstName(), getLastName(), getGrade(), getDepartment());
+	}
+	
+	// Equal
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		
+		Student other = (Student) obj;
+		
+		if (department == null) {
+			if (other.department != null) {
+				return false;
+			}
+		} else if (!department.equals(other.department)) {
+			return false;
+		}
+		
+		if (firstName == null) {
+			if (other.firstName != null) {
+				return false;
+			}
+		} else if (!firstName.equals(other.firstName)) {
+			return false;
+		}
+		
+		if (Double.doubleToLongBits(grade) != Double.doubleToLongBits(other.grade)) {
+			return false;
+		}
+		
+		if (lastName == null) {
+			if (other.lastName != null) {
+				return false;
+			}
+		} else if (!lastName.equals(other.lastName)) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	// Between 50 - 100
+	public boolean isGradeBetween50And100() {
+		if (getGrade() >= 50.0 && getGrade() <= 100.0) {
+			return true;
+		} else {
+			return false;
+		}
+	}	
+}
---
+++ JAC444/WS08/src/student/StudentProcess.java
@@ -0,0 +1,122 @@
+/*
+ * JAC444 Workshop 8
+ * File: StudentProcess.java
+ * Name: Youjin An (140413188)
+ * Date: July 22, 2020
+ */
+
+package student;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Consumer;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.Optional;
+
+public class StudentProcess {
+
+	public static void main(String[] args) {
+		
+		// Typical input
+		Student[] students = {
+				new Student("Jack", "Smith", 50.0, "IT"),
+				new Student("Aaron", "Johnson", 76.0, "IT"),
+				new Student("Maaria", "White", 35.8, "Business"),
+				new Student("John", "White", 47.0, "Media"),
+				new Student("Laney", "White", 62.0, "IT"),
+				new Student("Jack", "Jones", 32.9, "Business"),
+				new Student("Wesley", "Jones", 42.89, "Media")
+			};
+		
+		
+		List <Student> studentList = Arrays.asList(students);
+		
+		System.out.println("Task 1:\n");
+		System.out.println("Complete Student list:");
+		studentList.forEach(System.out::println);
+		
+		System.out.print("\n\n");
+		System.out.println("Task 2:\n");
+		System.out.println("Student who got 50.0-100.0 sorted by grade:");
+		studentList.stream()
+			.filter(Student::isGradeBetween50And100)
+			.sorted(Comparator.comparing(Student::getGrade))
+			.forEach(System.out::println);
+	
+		
+		System.out.print("\n\n");
+		System.out.println("Task 3:\n");
+		System.out.println("First Student who got 50.0-100.0:");
+		studentList.stream()
+			.filter(Student::isGradeBetween50And100)
+			.findFirst()
+			.ifPresent(System.out::println);
+		
+		System.out.print("\n\n");
+		System.out.println("Task 4:\n");
+		System.out.println("Students in ascending order by last name then first:");
+		studentList.stream()
+			.sorted(Comparator.comparing(Student::getLastName).thenComparing(Student::getFirstName))
+			.forEach(System.out::println);
+		System.out.println();
+		System.out.println("Students in descending order by last name then first:");
+		studentList.stream()
+			.sorted(Comparator.comparing(Student::getLastName).thenComparing(Student::getFirstName).reversed())
+			.forEach(System.out::println);
+		
+		System.out.print("\n\n");
+		System.out.println("Task 5:\n");
+		System.out.println("Unique Student last names:");
+		studentList.stream()
+			.map(Student::getLastName)
+			.distinct()
+			.sorted()
+			.forEach(System.out::println);
+		
+		System.out.print("\n\n");
+		System.out.println("Task 6:\n");
+		System.out.println("Student names in order by last name then first name:");
+		studentList.stream()
+			.sorted(Comparator.comparing(Student::getLastName).thenComparing(Student::getFirstName))
+			.map(Student::getName)
+			.forEach(System.out::println);
+		
+		System.out.print("\n\n");
+		System.out.println("Task 7:\n");
+		System.out.println("Students by department:");
+		Map<String, List<Student>> studentsByDepartment = studentList.stream().collect(Collectors.groupingBy(Student::getDepartment));
+		studentsByDepartment.forEach((department, studentsInDepartment) -> {
+			System.out.println(department);
+			studentsInDepartment.forEach(student -> {
+				System.out.println("   " + student.toString());
+			});
+		});
+		
+		System.out.print("\n\n");
+		System.out.println("Task 8:\n");
+		System.out.println("Count of Students by department:");
+		Map<String, Long> studentsByDepartment2 = studentList.stream().collect(Collectors.groupingBy(Student::getDepartment, TreeMap::new, Collectors.counting()));
+		studentsByDepartment2.forEach((department, numOfStudentsInDepartment) -> {
+			System.out.println(department + " has " + numOfStudentsInDepartment + " Student(s)");
+		});
+		
+		System.out.print("\n\n");
+		System.out.println("Task 9:\n");
+		System.out.print("Sum of Students' grades: ");
+		double sum = studentList.stream().mapToDouble(Student::getGrade).sum();
+		System.out.println(sum);
+		
+		System.out.print("\n\n");
+		System.out.println("Task 10:\n");
+		System.out.print("Average of Students' grades: ");
+		double average = studentList.stream().mapToDouble(Student::getGrade).average().getAsDouble();
+		System.out.println(String.format("%.2f", average));
+	}
+}
